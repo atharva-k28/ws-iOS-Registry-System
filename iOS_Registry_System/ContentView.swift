0a6d2a8b2903cbfc9beabc7c954a4c2cfacf58b8
Naman <Naman.bhansali@mitwpu.edu.in>
2026-05-16 16:00:54 +0530

    Initial Commit

---
+++ iOS_Registry_System/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_Registry_System
+//
+//  Created by SDC-USER on 16/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
